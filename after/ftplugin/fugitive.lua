8bf390abc8043096def77943610db42fe31cca29
Khoa Nguyen <dakennguyen@gmail.com>
2022-09-14 23:01:01 +0700

    update mapping

--- after/ftplugin/fugitive.lua
+++ after/ftplugin/fugitive.lua
@@ -2,10 +2,11 @@ buf_map('n', '<c-s>', 'o',  { noremap = false, nowait = true })
 buf_map('n', '<c-v>', 'gO', { noremap = false, nowait = true })
 buf_map('n', '<c-t>', 'O',  { noremap = false, nowait = true })
 
+buf_map('n', '<leader>0', ':e<cr>')
 buf_map('n', 'cl<space>', ':Git pull<space>')
 buf_map('n', 'cp<space>', ':Git push<space>')
 buf_map('n', 'ch<space>', ':Git fetch<space>')
-buf_map('n', 'cH<space>', ':Git remote update origin --prune<space>')
+buf_map('n', 'cH<space>', ':Git remote update origin --prune')
 buf_map('n', 'cd<space>', ':vert Git diff<space>')
 buf_map('n', 'W', ':Git commit --no-verify<CR>')
 buf_map('n', 'E', ':Git commit --amend --no-edit --no-verify')
@@ -17,12 +18,12 @@ buf_map('n', 'go', ':Merginal<CR>', { nowait = true })
 buf_map('n', '<leader>gd', ':vert Git diff origin/HEAD..HEAD<CR>',     { silent = true })
 
 vim.cmd[[
-function! s:open(cmd)
-  call execute(a:cmd)
-  quit
-  wincmd p
-endfunction
+"function! s:open(cmd)
+"  call execute(a:cmd)
+"  quit
+"  wincmd p
+"endfunction
 
-nnoremap <silent><buffer> <leader>gz :call <sid>open('Gclog! -g stash')<CR>
-nnoremap <silent><buffer> <leader>gc :call <sid>open('Gclog! -n 50')<CR>
+nnoremap <silent><buffer> <leader>gz :vsplit <bar> Gclog -g stash<CR>
+nnoremap <silent><buffer> <leader>gc :vsplit <bar> Gclog -n 50<CR>
 ]]
