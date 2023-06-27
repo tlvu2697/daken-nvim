b362938af8cc1f318733c2a79235c6d5b249018d
Khoa Nguyen <dakennguyen@gmail.com>
2023-06-27 10:36:07 +0200

    Update mappings

--- lua/plugins/vimscript/vsnip.lua
+++ lua/plugins/vimscript/vsnip.lua
@@ -2,12 +2,13 @@ return {
   'hrsh7th/vim-vsnip',
   event = 'InsertEnter',
   config = function()
-    vim.cmd([[
-    nmap        s   <Plug>(vsnip-select-text)
-    xmap        s   <Plug>(vsnip-select-text)
-
-    imap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
-    smap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
-    ]])
+    map('n', '<leader>v', '<Plug>(vsnip-select-text)', { noremap = false })
+    map('x', '<leader>v', '<Plug>(vsnip-select-text)', { noremap = false })
+    map('i', '<C-l>', [[vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>']], { noremap = false, expr = true })
+    map('s', '<C-l>', [[vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>']], { noremap = false, expr = true })
+    map('i', '<tab>', [[vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>']], { noremap = false, expr = true })
+    map('s', '<tab>', [[vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>']], { noremap = false, expr = true })
+    map('i', '<s-tab>', [[vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>']], { noremap = false, expr = true })
+    map('s', '<s-tab>', [[vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>']], { noremap = false, expr = true })
   end,
 }
